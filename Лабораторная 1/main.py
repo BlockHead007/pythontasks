3fc461fee5a06118092de3b8e3bceb9f9f7ce250
Block Head <katet2002@mail.ru>
2022-03-09 21:49:14 +0700

    Лабораторная 1 попытка 1

---
+++ Лабораторная 1/main.py
@@ -0,0 +1,89 @@
+import math
+
+def cons_input():
+    try:
+        a = float(input("Введите начало интегрирования: "))
+        try:
+            b = float(input("Введите конец интегрирования: "))
+            try:
+                eps = float(input("Введите точность вычисления: "))
+                return a, b, eps
+            except:
+                print("Ошибка чтения")
+                return None
+        except:
+            print("Ошибка чтения")
+            return None
+    except:
+        print("Ошибка чтения")
+        return None
+
+
+def integ(x: float) -> float:
+    f = 5*x**4+2*x**3/3+(-7)*x+13
+    return f
+
+
+def task(a, b, eps):
+    n = 1
+    h = (b - a) / n
+    s2 = integ(a + h*n) * h
+    s1 = 0
+    while math.fabs(s2-s1) <= eps:
+        s1 = s2
+        n *= 2
+        h *= n
+        s2 = 0
+        x = a + b / 2
+        for i in range(1, n):
+            s2 = s2 + integ(x)
+            x = x + h
+        s2 = s2 * h
+    return s2
+
+
+def file_read():
+    file = open("history.txt", "r+")
+    print(file.read())
+    file.close()
+
+
+def file_write(a: float, b: float, eps: float, w: float):
+    file = open("history.txt", "a+")
+    file.write("a = " + str(a) + "; b = " + str(b) + "; eps = " + str(eps) + "; w = " + str(w) + "\n")
+    file.close()
+
+
+def cons_output(x):
+    print(x)
+
+
+def main():
+    while True:
+        print("Доступные функции:\n"
+              "1. Посчитать интеграл\n"
+              "2. Просмотреть историю\n"
+              "0. Выход из программы\n")
+        try:
+            switch = int(input("Введите параметр: "))
+        except:
+            print("Ошибка чтения")
+            continue
+        match switch:
+            case 0:
+                break
+            case 1:
+                a, b, eps = cons_input()
+                w = task(a, b, eps)
+                print(f"Полученный результат: {w}\n")
+                file_write(a, b, eps, w)
+            case 2:
+                file_read()
+            case _:
+                print("Неверная функция!")
+
+
+if __name__ == '__main__':
+    main()
+    #3 функции: ввод, задание, вывод
+    #меню: решить уравнение, история, выход
